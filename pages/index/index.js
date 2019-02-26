feb036122b9dab5b132b2215eef322a1acc2748f
sssxt1 <34374266+sssxt1@users.noreply.github.com>
2019-02-26 17:17:26 +0800

    first

---
+++ pages/index/index.js
@@ -0,0 +1,13 @@
+Page({
+  data: {
+    imgUrls: [
+      'https://i2.hdslb.com/bfs/face/66c31cfdac40617600fa8ab1981dd932dc4e6d72.jpg',
+      'https://i1.hdslb.com/bfs/face/717376b9dbebcc68359444e4d0d90c46ea1e0b98.jpg',
+      'https://i2.hdslb.com/bfs/face/2996e22a24eed2d7767e452627a9130207defe6a.jpg',
+      '/img/png1.png',
+      '/img/png2.png'
+    ],
+    contentItems:[1,1,1,1],
+    listItems:[1,1,1,1,1,1,1,1,1,1]
+  }
+})
